da01e95f5ac010e52ab5b8330d3fa9d13fdbee9e
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-09-16 23:00:24 -0400

    commit #9

--- script.js
+++ script.js
@@ -5,43 +5,45 @@ const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
+const openingHours = {
+        [weekdays[3]]: {
+            open: 12,
+            close: 22,
+        },
+        [weekdays[4]]: {
+            open: 11,
+            close: 23,
+        },
+        [weekdays[5]]: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
+    };
+
 const restaurant = {
     name: "Classico Italiano",
     location: "Via Angelo Tavanti 23, Firenze, Italy",
     categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
     starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
     mainMenu: ["Pizza", "Pasta", "Risotto"],
+    openingHours,
 
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0, // Open 24 hours
-            close: 24,
-        },
-    },
-
-    order: function (starterIndex, mainIndex) {
+    order(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 
-    orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = "20:00", address }) {
         console.log(
             `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
         );
     },
 
-    orderPasta: function( ing1, ing2, ing3){
+    orderPasta( ing1, ing2, ing3){
         console.log(`Here is yoyur delicious pasta with ${ing1}, ${ing2}, and ${ing3}, `);
     },
     
-    orderPizza: function(firstIng, ...restIng){
+    orderPizza(firstIng, ...restIng){
         console.log(firstIng);
         console.log(restIng);
     }
@@ -56,8 +58,24 @@ const rest2 = {
     name: 'La Piazza',
     owner: 'Giovanni Rossi'
 };
-//Challenge #1
 
+/* 
+Let's continue with our football betting app!
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+GOOD LUCK 😀
+*/
 const game = {
   team1: "Bayern Munosh",
   team2: "Borrussia Dortmund",
@@ -99,6 +117,81 @@ const game = {
   },
 };
 
+// 1
+for(const [items, el] of game.scored?.entries()) console.log(`Goal ${items+1}, by ${el}`);
+
+//2
+const entries = Object.entries(game.odds)
+for(const [key, {team1, x, team2}] of entries){
+    console.log(`odds of ${key}`);
+}
+
+/**
+ * 
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open ${properties.length} days:`;
+
+for (const day of properties) {
+    openStr += `${day}`;
+}
+console.log(openStr);
+
+// Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entire Object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key, {open, close}] of entries) {
+    console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+ */
+
+/**
+if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// With Optional chaining
+console.log(restaurant.openingHours.mon?.open); //undefined
+console.log(restaurant.openingHours?.mon?.open); //undefined
+
+// Example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+const open = restaurant.openingHours[day]?.open ?? 'closed';
+console.log(`On ${day}, we are open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0,1)?? 'Method does not exist');
+console.log(restaurant.orderRessotto?.(0,1)?? 'Method does not exist');
+
+// Arrays
+const users = [
+    {name: 'Kuldeep', email: 'hell0@kuldeep.com'}
+]
+
+console.log(users[0]?.name ?? 'user array empty');
+
+if(users.length > 0) console.log(users[0]?.name);
+else console.log('user array empty');
+*/
+
+/*
+//looping through arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];;
+
+for(const item of menu) console.log(item);
+
+for (const [i,el] of menu.entries()){
+    console.log(`${i}: ${el}`);
+}
+*/
+
 // CHallenge #1
 /* 
 We're building a football betting app (soccer for my American friends 😅)!
@@ -112,8 +205,50 @@ Suppose we get data from a web service about a certain game (below). In this cha
 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
 TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 GOOD LUCK 😀
+
+const game = {
+  team1: "Bayern Munosh",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martiniz",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
 */
 
+/*
 [1] 
 //1. Create one player array for each team (variables 'players1' and 'players2')
 const [players1, players2] = game.players;// array destructuring
@@ -149,6 +284,7 @@ printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 team1 < team2 && console.log("team1 is more likely to win");
 team1 > team2 && console.log("team2 is more likely to win");
 // Challenge compleated
+*/
 
 // Logical OR assignment operator
 // rest1.numGuests = rest1.numGuests || 10;
