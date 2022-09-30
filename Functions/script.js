1b551006972381666bb5b161bf15fb236e6cb5cd
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-09-30 12:16:26 -0400

    commit 16

--- Functions/script.js
+++ Functions/script.js
@@ -1,2 +1,50 @@
 'use strict'
-//test
+
+const bookings = []
+
+const createBookings = function(flightNum, numPassengers = 1, price = 199 * numPassengers){
+    // ES5
+    // numPassengers = numPassengers || 1,
+    // price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBookings('LH123');
+createBookings('LH123',2,800);
+createBookings('LH123',2);
+createBookings('LH123',5);
+createBookings('LH123',undefined,1000);
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+  if (passenger.passport === 24739479284) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
