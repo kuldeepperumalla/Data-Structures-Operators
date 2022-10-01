05ac8e04d34e44cb9714136d516ab66225776caa
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-10-01 02:01:40 -0400

    commit #17

--- Functions/script.js
+++ Functions/script.js
@@ -48,3 +48,30 @@ const newPassport = function (person) {
 };
 newPassport(jonas);
 checkIn(flight, jonas); */
+
+// call back functions
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' -');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function(str, fn){
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('hello I\'m kuldeep', upperFirstWord);
+transformer('hello I\'m kuldeep', oneWord);
+
+const high5 =  function() {
+    console.log('✋');
+}
+document.body.addEventListener('click', high5);
+
+['a','b','c'].forEach(high5)
