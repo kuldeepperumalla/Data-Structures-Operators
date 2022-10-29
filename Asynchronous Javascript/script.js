8263f3543cb02958bef1df3bbb2cc1c356c818e7
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2022-10-29 18:24:09 -0400

    async #1

---
+++ Asynchronous Javascript/script.js
@@ -0,0 +1,127 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+// Ajax starts here
+
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+
+/* const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData("portugal");
+getCountryData("usa");
+getCountryData("germany"); */
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Render country 1
+    renderCountry(data);
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+// getCountryAndNeighbour('portugal');
+// Call back  hell example using setTimeout
+/* getCountryAndNeighbour("china");
+setTimeout(() => {
+  console.log("1 second passed");
+  setTimeout(() => {
+    console.log("2 seconds passed");
+    setTimeout(() => {
+      console.log("3 second passed");
+      setTimeout(() => {
+        console.log("4 second passed");
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000); */
+
+// Consuming Promises
+// Chaining Promises
+// Handling Rejected Promises
+// Throwing Errors Manually
+
+const getCountryData = (country) => { fetch(`https://restcountries.com/v2/name/${country}`)
+  .then((res) => res.json())
+  .then((data) => {renderCountry(data[0])});
+}
+
+
+getCountryData('usa');
+getCountryData('bharat');
+getCountryData('canada');
+getCountryData('france');
+getCountryData("usa");
+getCountryData("usa");
+getCountryData("usa");
+getCountryData("usa");
